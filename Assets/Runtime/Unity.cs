7d2e46e40707be3c0764bbdbfc257a3d1444f96a
Macchiato <simppp@gmail.com>
2025-02-21 15:45:32 +0900

    Hex값을 Color 객체로 반환하는 메소드 추가

--- Assets/Runtime/Unity.cs
+++ Assets/Runtime/Unity.cs
@@ -20,6 +20,18 @@ namespace VRSuya.Core {
 			Undo.SetCurrentGroupName(TargetUndoName);
 			return Undo.GetCurrentGroup();
 		}
+
+		/// <summary>HEX 문자열을 Color로 변환합니다.</summary>
+		/// <param name="HEXColorCode">변환할 HEX 문자열 (예: #RRGGBB)</param>
+		/// <returns>변환된 Color 객체</returns>
+		public static Color HexToColor(string HEXColorCode) {
+			if (HEXColorCode.StartsWith("#")) HEXColorCode = HEXColorCode.Substring(1);
+			if (HEXColorCode.Length != 6) return Color.black;
+			float R = int.Parse(HEXColorCode.Substring(0, 2), System.Globalization.NumberStyles.HexNumber) / 255f;
+			float G = int.Parse(HEXColorCode.Substring(2, 2), System.Globalization.NumberStyles.HexNumber) / 255f;
+			float B = int.Parse(HEXColorCode.Substring(4, 2), System.Globalization.NumberStyles.HexNumber) / 255f;
+			return new Color(R, G, B);
+		}
 	}
 }
 #endif
