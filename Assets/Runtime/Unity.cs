35436f2090d9d3ca225fe3e5ec6b47964ffd7c1a
Macchiato <simppp@gmail.com>
2025-02-21 15:16:04 +0900

    Undo 초기화 기능 추가

---
+++ Assets/Runtime/Unity.cs
@@ -0,0 +1,25 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+/*
+ * VRSuya Core
+ * Contact : vrsuya@gmail.com // Twitter : https://twitter.com/VRSuya
+ */
+
+namespace VRSuya.Core {
+
+	[ExecuteInEditMode]
+	public class Unity {
+
+		/// <summary>Unity Undo 그룹 기능을 초기화 합니다.</summary>
+		/// <param name="TargetUndoName">원하는 Undo 그룹 이름</param>
+		/// <returns>Undo 그룹 인덱스</returns>
+		public static int InitializeUndoGroup(string TargetUndoName) {
+			Undo.IncrementCurrentGroup();
+			Undo.SetCurrentGroupName(TargetUndoName);
+			return Undo.GetCurrentGroup();
+		}
+	}
+}
+#endif
